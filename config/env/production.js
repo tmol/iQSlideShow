2aa4e0a842196b43880a7293d23d2242cff8c0ca
tmol <tibor.edgar.molnar@gmail.com>
2015-06-18 21:42:13 +0000

    Fixed menu data-toggle issue

--- config/env/production.js
+++ config/env/production.js
@@ -17,7 +17,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/jquery/dist/jquery.js',                
+                'public/lib/bootstrap/dist/js/bootstrap.js'
 			]
 		},
 		css: [
