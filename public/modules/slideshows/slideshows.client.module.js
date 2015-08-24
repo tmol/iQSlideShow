3180d4305d334d43bff7431f58e8ed842c4ec28f
Eugen Rosenfeld <eugen.rosenfeld@iquestgroup.com>
2015-08-24 21:40:48 +0300

    fixed jslint

--- public/modules/slideshows/slideshows.client.module.js
+++ public/modules/slideshows/slideshows.client.module.js
@@ -1,4 +1,7 @@
-'use strict';
+/*global ApplicationConfiguration*/
+(function () {
+    'use strict';
 
-// Use applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('slideshows');
+    // Use applicaion configuration module to register a new module
+    ApplicationConfiguration.registerModule('slideshows');
+}());
