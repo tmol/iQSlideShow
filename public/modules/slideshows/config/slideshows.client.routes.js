878309453b2d4db0da4e041758d27eeaaa48e01e
Eugen Rosenfeld <eugen.rosenfeld@iquestgroup.com>
2015-08-24 21:10:35 +0300

    fixed JLint warnings

--- public/modules/slideshows/config/slideshows.client.routes.js
+++ public/modules/slideshows/config/slideshows.client.routes.js
@@ -1,29 +1,30 @@
-'use strict';
+(function () {
+    'use strict';
 
-//Setting up route
-angular.module('slideshows').config(['$stateProvider',
-	function($stateProvider) {
-		// Slideshows state routing
-		$stateProvider.
-		state('devices', {
-			url: '/devices',
-			templateUrl: 'modules/slideshows/views/devices.client.view.html'
-		}).
-		state('listSlideshows', {
-			url: '/slideshows',
-			templateUrl: 'modules/slideshows/views/list-slideshows.client.view.html'
-		}).
-		state('createSlideshow', {
-			url: '/slideshows/create',
-			templateUrl: 'modules/slideshows/views/create-slideshow.client.view.html'
-		}).
-		state('viewSlideshow', {
-			url: '/slideshows/:slideshowId',
-			templateUrl: 'modules/slideshows/views/view-slideshow.client.view.html'
-		}).
-		state('editSlideshow', {
-			url: '/slideshows/:slideshowId/edit',
-			templateUrl: 'modules/slideshows/views/edit-slideshow.client.view.html'
-		});
-	}
-]);
+    //Setting up route
+    window.angular.module('slideshows').config(['$stateProvider',
+        function ($stateProvider) {
+            // Slideshows state routing
+            $stateProvider.
+                state('devices', {
+                    url: '/devices',
+                    templateUrl: 'modules/slideshows/views/devices.client.view.html'
+                }).
+                state('listSlideshows', {
+                    url: '/slideshows',
+                    templateUrl: 'modules/slideshows/views/list-slideshows.client.view.html'
+                }).
+                state('createSlideshow', {
+                    url: '/slideshows/create',
+                    templateUrl: 'modules/slideshows/views/create-slideshow.client.view.html'
+                }).
+                state('viewSlideshow', {
+                    url: '/slideshows/:slideshowId',
+                    templateUrl: 'modules/slideshows/views/view-slideshow.client.view.html'
+                }).
+                state('editSlideshow', {
+                    url: '/slideshows/:slideshowId/edit',
+                    templateUrl: 'modules/slideshows/views/edit-slideshow.client.view.html'
+                });
+        }]);
+}());
--- public/modules/slideshows/controllers/devices.client.controller.js
+++ public/modules/slideshows/controllers/devices.client.controller.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
-    var angular = window.angular || {};
-    angular.module('slideshows').controller('DevicesController', ['$scope', 'Devices',
+    window.angular.module('slideshows').controller('DevicesController', ['$scope', 'Devices',
         function ($scope, Devices) {
             Devices.get(function (res) {
                 $scope.devices = res.devices;
