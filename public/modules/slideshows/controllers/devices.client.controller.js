5541f953510ae5953d57d4aaf050dc4c6c2e495a
Eugen Rosenfeld <eugen.rosenfeld@iquestgroup.com>
2015-08-24 21:05:27 +0300

    fixed JSLint warnings

--- public/modules/slideshows/controllers/devices.client.controller.js
+++ public/modules/slideshows/controllers/devices.client.controller.js
@@ -1,9 +1,10 @@
-'use strict';
-
-angular.module('slideshows').controller('DevicesController', ['$scope', 'Devices',
-	function($scope, Devices) {
-		Devices.get(function(res) {
-            $scope.devices = res.devices;
-        });
-	}
-]);
+(function () {
+    'use strict';
+    var angular = window.angular || {};
+    angular.module('slideshows').controller('DevicesController', ['$scope', 'Devices',
+        function ($scope, Devices) {
+            Devices.get(function (res) {
+                $scope.devices = res.devices;
+            });
+        }]);
+}());
