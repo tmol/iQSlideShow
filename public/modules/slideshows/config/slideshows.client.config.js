e6a280f3054bc3613cdd6801ee84031d597c9a43
Eugen Rosenfeld <eugen.rosenfeld@iquestgroup.com>
2015-08-24 21:08:30 +0300

    fixe jslint warnings

--- public/modules/slideshows/config/slideshows.client.config.js
+++ public/modules/slideshows/config/slideshows.client.config.js
@@ -1,11 +1,12 @@
-'use strict';
-
-// Configuring the Articles module
-angular.module('slideshows').run(['Menus',
-	function(Menus) {
-		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Slideshows', 'slideshows', 'dropdown', '/slideshows(/create)?');
-		Menus.addSubMenuItem('topbar', 'slideshows', 'List Slideshows', 'slideshows');
-		Menus.addSubMenuItem('topbar', 'slideshows', 'New Slideshow', 'slideshows/create');
-	}
-]);
+(function () {
+    'use strict';
+    // Configuring the Articles module
+    window.angular.module('slideshows').run(['Menus',
+        function (Menus) {
+            // Set top bar menu items
+            Menus.addMenuItem('topbar', 'Slideshows', 'slideshows', 'dropdown', '/slideshows(/create)?');
+            Menus.addSubMenuItem('topbar', 'slideshows', 'List Slideshows', 'slideshows');
+            Menus.addSubMenuItem('topbar', 'slideshows', 'New Slideshow', 'slideshows/create');
+        }
+        ]);
+}());
