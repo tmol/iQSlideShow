bd3be6a6dff07a2e493c5b198f11cb16f106544a
Eugen Rosenfeld <eugen.rosenfeld@iquestgroup.com>
2015-08-24 21:27:49 +0300

    fixed jslint

--- public/modules/slideshows/services/controller.client.service.js
+++ public/modules/slideshows/services/controller.client.service.js
@@ -1,10 +1,11 @@
-'use strict';
+(function () {
+    'use strict';
 
-//Devices service used to communicate Devices REST endpoints
-angular.module('slideshows').factory('Devices', ['$resource',
-	function($resource) {
-		return $resource('devices',  {
-            getDevices: { method: 'GET', isArray: true }
-        });
-	}
-]);
+    //Devices service used to communicate Devices REST endpoints
+    window.angular.module('slideshows').factory('Devices', ['$resource',
+        function ($resource) {
+            return $resource('devices',  {
+                getDevices: { method: 'GET', isArray: true }
+            });
+        }]);
+}());
