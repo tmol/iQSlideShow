4284ad3c543c3b549d2044ac62b55b00290b974b
Eugen Rosenfeld <eugen.rosenfeld@iquestgroup.com>
2015-08-25 19:55:14 +0300

    Added the device id to deviceInteraction's $scope

--- public/modules/slideshows/controllers/deviceInteraction.client.controller.js
+++ public/modules/slideshows/controllers/deviceInteraction.client.controller.js
@@ -3,12 +3,12 @@
     'use strict';
     angular.module('slideshows').controller('DeviceInteractionController', ['$scope', '$state', '$stateParams', 'Slides',
         function ($scope, $state, $stateParams, Slides) {
+            $scope.deviceId = $stateParams.deviceId;
             Slides.get({slideId : $stateParams.slideshowId, slideNumber : $stateParams.slideNumber}, function (slide) {
-                    $scope.slideUrl = slide.detailsUrl || $state.href("player", {
-                        slideName : $stateParams.slideshowId,
-                        slideNumber : $stateParams.slideNumber
-                    }, {absolute : true});
-
-			     })
+                $scope.slideUrl = slide.detailsUrl || $state.href("player", {
+                    slideName : $stateParams.slideshowId,
+                    slideNumber : $stateParams.slideNumber
+                }, {absolute : true});
+            });
         }]);
 }());
