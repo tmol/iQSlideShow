9ab1083a5c11702f8a3780f2cd8c17948bb2da82
rosenfeldeugen <Eugen.Rosenfeld@iquestgroup.com>
2015-09-05 16:40:10 +0300

    1. Added Path service. this is useful to obtain an URL for different files (currently Views)
    2. Extracted waitingForActivation template
    3. Added NodeJS visual studio project (https://www.visualstudio.com/en-us/features/node-js-vs.aspx)

---
+++ public/modules/core/services/path.js
@@ -0,0 +1,10 @@
+﻿/*global angular*/
+(function () {
+    'use strict';
+    angular.module('core').service('Path', ['$state',
+        function ($state) {
+            this.getViewUrl = function (view) {
+                return $state.current.templateUrl + '/../' + view + '.html';
+            };
+        }]);
+}());
