2070992df677cb03a9e19f62a9caa8515de010d2
rosenfeldeugen <Eugen.Rosenfeld@iquestgroup.com>
2015-09-13 20:51:53 +0300

    removed unused controller

--- public/modules/player/controllers/slide.client.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-angular.module('player').controller('SlideController',['$scope','$stateParams','$timeout',
-    function($scope,$stateParams,$timeout){
-        if (!$stateParams.slide) return;
-        $scope.slide=$stateParams.slide;
-    }
-])
