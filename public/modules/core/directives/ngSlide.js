4fb2a2fc64b2bd3c4911acb6f1fc808122c62527
rosenfeldeugen <Eugen.Rosenfeld@iquestgroup.com>
2015-09-20 17:45:36 +0800

    moved ngSlide to core

--- public/modules/core/directives/ngSlide.js
+++ public/modules/core/directives/ngSlide.js
@@ -1,7 +1,7 @@
 'use strict';
-angular.module('player').directive('ngSlide', [
+angular.module('core').directive('ngSlide', [
 	function() {
-        
+
         return {
             link: function postLink(scope, element, attrs) {
                 scope.$parent.templateElements=scope.$parent.templateElements||{};
@@ -10,18 +10,18 @@ angular.module('player').directive('ngSlide', [
                     label:attrs.label||attrs.member
                 };
                 var content = scope.$parent.slide || scope.$parent.currentSlide.content;
-                
+
                 scope.$parent.$watch(function(){
                     if (!content){
                         return;
                     }
-                    
+
                     if (element[0].tagName=="IMG")
                     {
                         element[0].src=content[attrs.member] || 'modules/slideshows/css/img/default.jpg';
                         return;
                     }
-                                   
+
                     element[0].innerHTML=content[attrs.member];
                 });
 			}
