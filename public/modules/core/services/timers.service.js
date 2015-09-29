516e479e71ed517b429db277454a960abfb27c3e
rosenfeldeugen <Eugen.Rosenfeld@iquestgroup.com>
2015-09-30 02:48:10 +0800

    Add interval capabilities in timers.service

--- public/modules/core/services/timers.service.js
+++ public/modules/core/services/timers.service.js
@@ -4,19 +4,30 @@
     'use strict';
 
     // Users service used for communicating with the users REST endpoint
-    angular.module('core').factory('Timers', ['$timeout',
-        function ($timeout) {
+    angular.module('core').factory('Timers', ['$timeout', '$interval',
+        function ($timeout, $interval) {
             return function () {
                 var timeoutCollection = {};
+                var intervalCollection = {};
 
                 this.registerTimeout = function (key, func, delay) {
                     $timeout.cancel(timeoutCollection[key]);
                     timeoutCollection[key] = $timeout(func, delay);
                 };
 
+                this.registerInterval = function (key, func, delay) {
+                    $interval.cancel(intervalCollection[key]);
+                    intervalCollection[key] = $interval(func, delay);
+                };
+
                 this.unRegisterTimeout = function (key) {
                     $timeout.cancel(timeoutCollection[key]);
                     delete timeoutCollection[key];
+                };
+
+                this.unRegisterInterval = function (key) {
+                    $interval.cancel(intervalCollection[key]);
+                    delete intervalCollection[key];
                 };
 
                 this.resetTimeouts = function () {
@@ -27,6 +38,16 @@
                         }
                     }
                     timeoutCollection = {};
+                };
+
+                this.resetIntervals = function () {
+                    var prop;
+                    for (prop in intervalCollection) {
+                        if (intervalCollection.hasOwnProperty(prop)) {
+                            $interval.cancel(intervalCollection[prop]);
+                        }
+                    }
+                    intervalCollection = {};
                 };
             };
         }]);
