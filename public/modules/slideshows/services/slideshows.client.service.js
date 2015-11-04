bd5644d859ccb3639666afe46fda03d3610bb374
rosenfeldeugen <Eugen.Rosenfeld@iquestgroup.com>
2015-11-04 22:22:02 +0200

    Add name as parameter when geting devices for a slideshow

--- public/modules/slideshows/services/slideshows.client.service.js
+++ public/modules/slideshows/services/slideshows.client.service.js
@@ -5,10 +5,22 @@
     //Slideshows service used to communicate Slideshows REST endpoints
     angular.module('slideshows').factory('Slideshows', ['$resource',
         function ($resource) {
-            return $resource('slideshows/:slideshowId',
-                             { slideshowId: '@_id' },
-                             { update:
-                              {method: 'PUT'}
-                             });
+            return $resource('slideshows/:slideshowId', {
+                slideshowId: '@_id'
+            }, {
+                update: {
+                    method: 'PUT'
+                },
+                getDevices: {
+                    method: 'GET',
+                    url: 'slideshowDevices/:slideshowId',
+                    isArray: true,
+                    params: {name: '@name'}
+                },
+                setDevices: {
+                    method: 'PUT',
+                    url: 'slideshowDevices/:slideshowId'
+                }
+            });
         }]);
 }());
