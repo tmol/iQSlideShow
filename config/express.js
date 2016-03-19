c8d7a002493b551a9be5d53e90c2413324cc06fb
rosenfeldeugen <Eugen.Rosenfeld@iquestgroup.com>
2016-03-19 10:53:51 +0200

    moved the static publisher middleware before session creation in order to increase the priority

--- config/express.js
+++ config/express.js
@@ -44,6 +44,8 @@
         app.locals.messageChannelName = config.getMessageChannelName();
         app.locals.appVersion = config.getAppVersion();
 
+
+
         // Passing the request url to environment locals
         app.use(function (req, res, next) {
             res.locals.url = req.protocol + '://' + req.headers.host + req.url;
@@ -90,6 +92,10 @@
         // CookieParser should be above session
         app.use(cookieParser());
 
+        // Setting the app router and static folder
+        app.use(express.static(path.resolve('./public')));
+        app.use(express.static(path.resolve('./config/shared')));
+
         // Express MongoDB session storage
         app.use(session({
             saveUninitialized: true,
@@ -115,9 +121,7 @@
         app.use(helmet.ienoopen());
         app.disable('x-powered-by');
 
-        // Setting the app router and static folder
-        app.use(express.static(path.resolve('./public')));
-        app.use(express.static(path.resolve('./config/shared')));
+
 
 
         // Globbing routing files
