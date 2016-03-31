632bcfeb558d2ddc315507c7b59f4c2b4854c3af
tmol <tibor.edgar.molnar@gmail.com>
2016-03-31 11:17:31 +0300

    Small refactoring, common scroll bar detector directive extarcted in core module.

--- public/modules/core/directives/domElementScrollBarDetector.js
+++ public/modules/core/directives/domElementScrollBarDetector.js
@@ -1,6 +1,6 @@
 /*global angular*/
 /*jslint nomen: true, es5: true */
-angular.module('slideshows').directive('devicesToPlayOnDivScrollBarDetector', ['$timeout', '$rootScope', function ($timeout, $rootScope) {
+angular.module('core').directive('domElementScrollBarDetector', ['$timeout', '$rootScope', function ($timeout, $rootScope) {
     'use strict';
 
     function link(scope, element, attrs) {
