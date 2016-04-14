0ba0523fe48d80b3a5f050c3516dc9fa9e3dd230
tmol <tibor.edgar.molnar@gmail.com>
2016-04-14 08:18:00 +0300

    Fix [IQSLDSH-149].

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -4,6 +4,13 @@
     // Setting up route
     angular.module('core').config(['$stateProvider', '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
+            $stateProvider.state('terms', {
+                url: '/terms',
+                templateUrl: 'modules/core/views/terms.html'
+            }).state('privacy', {
+                url: '/privacy',
+                templateUrl: 'modules/core/views/privacy.html'
+            })
             // Redirect to home view when route not found
             $urlRouterProvider.otherwise('/slideshows');
         }
