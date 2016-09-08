460917bb1caee74399863a8b5f9d6e160348ef41
Mircea Chirea <Mircea.Chirea@iquestgroup.com>
2016-09-08 17:49:27 +0300

    Fix indentation in the text-fill directive.

--- public/modules/core/directives/textFill.js
+++ public/modules/core/directives/textFill.js
@@ -6,35 +6,35 @@
     // max-font-size attribute = maximum font size in pixels
     // Directive must be used on the parent of an element with the .text-fill class
     angular.module('core').directive('textFill', ["$timeout", function ($timeout) {
-            return {
-                link: function postLink(scope, element, attrs) {
-                    if ($(element).find('.text-fill').length === 0) {
-                        return;
-                    }
-
-                    var minFontSize = parseFloat(attrs.minFontSize);
-                    var maxFontSize = parseFloat(attrs.maxFontSize);
-
-                    if (!minFontSize || !maxFontSize) {
-                        return;
-                    }
-
-                    var applyTextFill = function() {
-                        $(element).textfill({
-                            minFontPixels: minFontSize,
-                            maxFontPixels: maxFontSize,
-                            innerTag: '.text-fill'
-                        });
-                    }
-
-                    scope.$on('updateSlideContentPart', function (event, content, member, slidePartId) {
-                        if (member === attrs.member) {
-                            applyTextFill();
-                        }
-                    });
-
-                    $timeout(applyTextFill, 100);
+        return {
+            link: function postLink(scope, element, attrs) {
+                if ($(element).find('.text-fill').length === 0) {
+                    return;
                 }
-            };
-        }]);
+
+                var minFontSize = parseFloat(attrs.minFontSize);
+                var maxFontSize = parseFloat(attrs.maxFontSize);
+
+                if (!minFontSize || !maxFontSize) {
+                    return;
+                }
+
+                var applyTextFill = function() {
+                    $(element).textfill({
+                        minFontPixels: minFontSize,
+                        maxFontPixels: maxFontSize,
+                        innerTag: '.text-fill'
+                    });
+                }
+
+                scope.$on('updateSlideContentPart', function (event, content, member, slidePartId) {
+                    if (member === attrs.member) {
+                        applyTextFill();
+                    }
+                });
+
+                $timeout(applyTextFill, 100);
+            }
+        };
+    }]);
 }());
