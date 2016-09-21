20deb42cf69cbe9e536cf43e093fa254d101d47e
Mircea Chirea <Mircea.Chirea@iquestgroup.com>
2016-09-21 15:49:37 +0300

    Fix massive SPAM of errors from QR code in the browser console.

--- public/modules/core/directives/qrDeviceInteraction.js
+++ public/modules/core/directives/qrDeviceInteraction.js
@@ -4,7 +4,7 @@
 
     angular.module('core').directive('qrDeviceInteraction', function () {
         var template = '';
-        template += '<a href="{{qrConfig.slideUrl}}" target="_blank">';
+        template += '<a href="{{qrConfig.slideUrl}}" target="_blank" ng-if="qrConfig">';
         template += '    <qr text="qrConfig.slideUrl" type-number="qrConfig.typeNumber" correction-level="qrConfig.correctionLevel"';
         template += '        size="qrConfig.size" input-mode="qrConfig.inputMode" image="qrConfig.image">';
         template += '    </qr>';
@@ -12,7 +12,7 @@
 
         return {
             restrict: 'AE',
-            template: template,
+            template: template
         };
     });
 }());
