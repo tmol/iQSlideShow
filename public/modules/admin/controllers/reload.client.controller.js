5976ec4f3de26af0f69de12ab0523766bb706a8f
Mircea Chirea <Mircea.Chirea@iquestgroup.com>
2016-09-30 16:13:35 +0300

    Fix not logged-in users being able to access the reload page. [IQSLDSH-364]

--- public/modules/admin/controllers/reload.client.controller.js
+++ public/modules/admin/controllers/reload.client.controller.js
@@ -1,7 +1,10 @@
 'use strict';
 
-angular.module('admin').controller('ReloadController', ['$scope', 'Admin',
-	function($scope, Admin) {
+angular.module('admin').controller('ReloadController', ['$scope', '$location', 'Authentication', 'Admin',
+	function($scope, $location, Authentication, Admin) {
+        // If user is not signed in then redirect back home
+        if (!Authentication.user) $location.path('/');
+
 		 $scope.reload = function () {
              Admin.reload(function(res) {
                  $scope.reloadedDevices = res.reloadedDevices;
