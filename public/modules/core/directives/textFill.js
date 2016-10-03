0bdd128e405748e4807152ef4177507950e348ad
Mircea Chirea <Mircea.Chirea@iquestgroup.com>
2016-10-03 15:11:16 +0300

    Rewrite text-fill directive to use isolated scope. [IQSLDSH-382]

--- public/modules/core/directives/textFill.js
+++ public/modules/core/directives/textFill.js
@@ -2,33 +2,32 @@
 (function () {
     'use strict';
 
+    // member attribute = member in slide to watch for changes
     // min-font-size attribute = minimum font size in pixels
     // max-font-size attribute = maximum font size in pixels
     // Directive must be used on the parent of an element with the .text-fill class
     angular.module('core').directive('textFill', ["$timeout", function ($timeout) {
         return {
+            scope: {
+                member: "@",
+                minFontSize: "=",
+                maxFontSize: "=",
+            },
             link: function postLink(scope, element, attrs) {
                 if ($(element).find('.text-fill').length === 0) {
                     return;
                 }
 
-                var minFontSize = parseFloat(attrs.minFontSize);
-                var maxFontSize = parseFloat(attrs.maxFontSize);
-
-                if (!minFontSize || !maxFontSize) {
-                    return;
-                }
-
                 var applyTextFill = function() {
                     $(element).textfill({
-                        minFontPixels: minFontSize,
-                        maxFontPixels: maxFontSize,
+                        minFontPixels: scope.minFontSize,
+                        maxFontPixels: scope.maxFontSize,
                         innerTag: '.text-fill'
                     });
                 }
 
                 scope.$on('updateSlideContentPart', function (event, content, member, slidePartId) {
-                    if (member === attrs.member) {
+                    if (member === scope.member) {
                         $timeout(applyTextFill, 100, false);
                     }
                 });
